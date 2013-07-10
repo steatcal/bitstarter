58fbee32145ef2fbb2778de5d580d8e63462a27b
steatcal <ste.at.cal@gmail.com>
2013-07-10 15:59:44 +0000

    messing with some stuff

--- web.js
+++ web.js
@@ -1,10 +1,19 @@
 var express = require('express');
+var fs = require('fs');
+var buf = new Buffer(80);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World! Hallo Welt!');
 });
+
+buf = fs.readFile('~index.html');
+app.get('/', function(request, response) {
+  response.send(buf);
+});
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
