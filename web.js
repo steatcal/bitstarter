31d02919f2136824f3928b443968c37dfa7f5795
steatcal <ste.at.cal@gmail.com>
2013-08-07 05:36:26 +0000

    changed web.js to the one in pdf pg14

--- web.js
+++ web.js
@@ -1,19 +1,22 @@
 var express = require('../node-js-sample/node_modules/express');
+//var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var buf = new Buffer(10000);
+//var buf = new Buffer(10000);
 
-buf = fs.readFileSync('index.html');
+//buf = fs.readFileSync('index.html');
 // console.log(buf.toString());
 
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
+//  response.send(buf.toString('utf-8'));
+    var html = fs.readFileSync('index.html').toString();
+    response.send(html);
 });
 
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
